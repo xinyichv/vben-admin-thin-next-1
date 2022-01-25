abce7a8064e5756db0cac7b0e3425f5375982718
凌云文档 <xinyichv@163.com>
2022-01-25 10:13:29 +0800

    feat: 路由更新

---
+++ src/api/system/thirdparty.ts
@@ -0,0 +1,24 @@
+import { ContentTypeEnum } from '/@/enums/httpEnum';
+import { defHttp } from '/@/utils/http/axios';
+
+enum Api {
+  SYS_SETTING = '/idoc/common/system/syssetting',
+  BASE_CONFIG = '/idoc/pages/sysconfig/baseconfig',
+}
+
+export function getSysSetting(params: any) {
+  return defHttp.post<any>({
+    url: Api.SYS_SETTING,
+    params,
+  });
+}
+
+export function saveSysSetting(params: any) {
+  return defHttp.post<any>({
+    url: Api.BASE_CONFIG,
+    params,
+    headers: {
+      'Content-type': ContentTypeEnum.FORM_DATA,
+    },
+  });
+}
